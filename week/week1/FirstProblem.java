b43fa4a6c7ba5c007096e90487e67120be9ba441
nivethakanmani <nivethaprabakaran2913@gmail.com>
2025-12-20 12:13:25 +0530

    first program

--- week/week1/FirstProblem.java
+++ week/week1/FirstProblem.java
@@ -1,3 +1,25 @@
-public class FirstProblem {
 
-}
+import java.util.Scanner;
+public class FirstProblem {
+    public static void main(String[] args){
+
+        Scanner scan=new Scanner(System.in);
+
+        int num= scan.nextInt();
+        scan.nextLine(); // to consume the newline character after the integer input
+        String str= scan.next();
+    
+        char ch=scan.next().charAt(0);
+        System.out .println(ch);
+        System.out .println(num);
+        System.out .println(str);
+
+
+
+    }
+
+    }
+
+
+//Prob 1 : Write a program that takes an integer, then a string, 
+// then a char from the user and prints them in the screen.
